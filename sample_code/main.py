bbbb80bc60427ea8d293aef4a3101a3925c00089
段恒斌 <henryduan.com@outlook.com>
2023-08-14 22:31:16 +0800

    first

---
+++ sample_code/main.py
@@ -0,0 +1,20 @@
+# a = [10, 8, 7, 1, 9, 5, 10, 3]
+# print(sum(a))
+# print(len(a))
+#
+# *a1, b1 = a
+# print(a1)
+
+# class A:
+#     def __init__(self,name):
+#         self.name = name
+#
+#
+# class B(A):
+#     def __init__(self):
+#         print(f"{self.name}, bbbbb")
+#
+# b = B("kele")
+
+a = {}
+print(type(a))
